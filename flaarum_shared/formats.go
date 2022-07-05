ca9ff3c8b8198afec0e39000bced702627f37101
Bankole Ojo <ojobankole@gmail.com>
2022-07-05 03:58:34 +0100

    merged data_formats.go and formats.go

--- flaarum_shared/formats.go
+++ flaarum_shared/formats.go
@@ -112,3 +112,70 @@ func ReadPortionF2File(projName, tableName, name string, begin, end int64) ([]by
 
   return outData, nil
 }
+
+
+func ParseEncodedRowData(rawData []byte) (map[string]string, error) {
+  ret := make(map[string]string)
+  nl := GetNewline()
+
+  partsOfRawData := strings.Split(string(rawData), nl)
+  for _, line := range partsOfRawData {
+    var colonIndex int
+    for i, ch := range line {
+			if fmt.Sprintf("%c", ch) == ":" {
+				colonIndex = i
+				break
+			}
+		}
+
+    if colonIndex == 0 {
+      continue
+    }
+
+    optName := strings.TrimSpace(line[0:colonIndex])
+    optValue := strings.TrimSpace(line[colonIndex + 1: ])
+
+    ret[optName] = optValue
+
+  }
+
+  rawDataStr := string(rawData)
+  for k, v := range ret {
+    if strings.TrimSpace(v) == "" {
+      firstIndex := strings.Index(rawDataStr, fmt.Sprintf("%s%s:", nl, k))
+      lastIndex := strings.LastIndex(rawDataStr, fmt.Sprintf("%s%s:", nl, k))
+      padding := len( fmt.Sprintf("%s%s:", nl, k))
+      if firstIndex != lastIndex {
+        ret[k] = rawDataStr[firstIndex+padding: lastIndex]
+      }
+    }
+  }
+
+  return ret, nil
+}
+
+
+func ParseDataFormat(path string) (map[string]string, error) {
+  raw, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+  return ParseEncodedRowData(raw)
+}
+
+
+func EncodeRowData(projName, tableName string, toWrite map[string]string) string {
+  nl := GetNewline()
+  out := nl
+  for k, v := range toWrite {
+    ft := GetFieldType(projName, tableName, k)
+    if ft == "text" {
+    out += fmt.Sprintf("%s:%s%s%s%s:%s", k, nl, v, nl, k, nl)
+    } else {
+      out += fmt.Sprintf("%s: %s%s", k, v, nl)
+    }
+  }
+
+  return out
+}
