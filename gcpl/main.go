89c3c54610b4785bd99cea7795ec3eef9fe46458
Bankole Ojo <ojobankole@gmail.com>
2023-02-25 21:29:55 +0100

    Revert "updated gcpl subprogram"
    This reverts commit 50fe73e2f2889eec74ee1d86bebe940afd75c004.

--- gcpl/main.go
+++ gcpl/main.go
@@ -181,7 +181,6 @@ sudo snap start flaarum.store
 sudo flaarum.prod mpr
 sudo snap restart flaarum.store
 sudo snap stop --disable flaarum.statsr
-sudo snap stop --disable flaarum.gcpasr
 `
 
 		ctx := context.Background()
